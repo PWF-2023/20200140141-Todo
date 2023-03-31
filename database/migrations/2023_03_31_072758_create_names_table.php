cb9c65e196cb06c2abd89e7d59e1b0a414bf2803
Nur Sakinah <nursakinah2402@gmail.com>
2023-03-31 19:05:07 +0700

    migration, database seeder, factory

--- database/migrations/2023_03_31_072758_create_names_table.php
+++ database/migrations/2023_03_31_072758_create_names_table.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('names', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
-            $table->boolean('is_admin')->default(false);
         });
     }
 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('names');
     }
 };
